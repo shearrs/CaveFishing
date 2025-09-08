c965c9d52bdd0d3b1f568c04a02dd35803fee1ce
reagancheatham <reagancheatham@gmail.com>
2025-09-08 09:50:19 -0500

    Add rocks and fishing rod functionality

--- Assets/Scripts/Cave Fishing/Fishing/Bobber.cs
+++ Assets/Scripts/Cave Fishing/Fishing/Bobber.cs
@@ -5,6 +5,18 @@ namespace CaveFishing.Fishing
     [RequireComponent(typeof(Rigidbody), typeof(SphereCollider))]
     public class Bobber : MonoBehaviour
     {
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            rb.position = position;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log("bobber trigger");
--- Assets/Scripts/Cave Fishing/Fishing/FishingRod.cs
+++ Assets/Scripts/Cave Fishing/Fishing/FishingRod.cs
@@ -4,10 +4,21 @@ namespace CaveFishing.Fishing
 {
     public class FishingRod : MonoBehaviour
     {
-        // sends out a bobber and listens for the results
+        [SerializeField] private Bobber bobber;
+        [SerializeField] private Vector3 castPosition;
+
+        private Vector3 CastPosition => transform.TransformPoint(castPosition);
+
         public void Cast()
         {
+            bobber.gameObject.SetActive(true);
+            bobber.SetPosition(CastPosition);
+        }
 
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(CastPosition, 0.35f);
         }
     }
 }
