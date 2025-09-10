be85f1382b5f41d4f1d8145c580a7395c702dc1b
reagancheatham <reagancheatham@gmail.com>
2025-09-10 18:33:40 -0500

    Add fishing rod charge

--- Assets/Scripts/Cave Fishing/Fishing/FishingRod.cs
+++ Assets/Scripts/Cave Fishing/Fishing/FishingRod.cs
@@ -12,12 +12,15 @@ namespace CaveFishing.Fishing
         [SerializeField] private Transform castPoint;
 
         [Header("Cast Force Settings")]
-        [SerializeField] private float forwardCastForce = 10f;
-        [SerializeField] private float upCastForce = 5f;
+        [SerializeField] private float minForwardCastForce = 0.5f;
+        [SerializeField] private float maxForwardCastForce = 3f;
+        [SerializeField] private float minUpCastForce = 0.5f;
+        [SerializeField] private float maxUpCastForce = 3f;
 
         [Header("Tween Settings")]
         [SerializeField] private float releaseRotation = 20f;
         [SerializeField] private float chargeRotation = -24f;
+        [SerializeField, Range(0f, 1f)] private float releaseTime = 0.85f;
         [SerializeField] private StructTweenData chargeTweenData;
         [SerializeField] private StructTweenData releaseTweenData;
 
@@ -33,20 +36,24 @@ namespace CaveFishing.Fishing
 
         public void Cast()
         {
+            float chargeProgress = tween.Progress;
+            float forwardForce = Mathf.Lerp(minForwardCastForce, maxForwardCastForce, chargeProgress);
+            float upForce = Mathf.Lerp(minUpCastForce, maxUpCastForce, chargeProgress);
+
             Vector3 eulerRotation = transform.localEulerAngles;
             eulerRotation.x = releaseRotation;
 
             tween?.Dispose();
             tween = transform.DoRotateLocalTween(Quaternion.Euler(eulerRotation), true, releaseTweenData);
-            tween.AddOnComplete(OnCastTweenComplete);
+            tween.AddEvent(releaseTime, () => OnCastTweenComplete(forwardForce, upForce));
         }
 
-        private void OnCastTweenComplete()
+        private void OnCastTweenComplete(float forwardForce, float upForce)
         {
             Vector3 forward = castPoint.forward;
             forward.y = 0;
             forward.Normalize();
-            Vector3 force = forwardCastForce * forward + upCastForce * castPoint.up;
+            Vector3 force = forwardForce * forward + upForce * castPoint.up;
 
             bobber.gameObject.SetActive(true);
             bobber.Cast(castPoint.position, force);
